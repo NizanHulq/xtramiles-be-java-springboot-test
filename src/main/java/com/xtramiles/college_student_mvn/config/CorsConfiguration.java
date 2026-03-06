d12da916fd6226940ff64e50752021b341f6ceb2
Nizan Dhiaulhaq <nizandhiaulhaq@mail.ugm.ac.id>
2026-03-07 01:54:57 +0700

    feat: Add global CORS configuration allowing all origins, methods, and headers.

---
+++ src/main/java/com/xtramiles/college_student_mvn/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.xtramiles.college_student_mvn.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
